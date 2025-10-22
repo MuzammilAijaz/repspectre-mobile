4b914b0c10a791f48321007d88a2035e0fef71bd
MuzammilAijaz <171587778+MuzammilAijaz@users.noreply.github.com>
2025-10-22 17:43:01 +0500

    add sample BLE modules for reference
    These modules are from android studio github page :
    https://github.com/android/platform-samples/tree/main/samples/connectivity/bluetooth/ble

--- app/src/main/java/com/example/esp32_mpu6050_mobile_data_collection/MainActivity.kt
+++ app/src/main/java/com/example/esp32_mpu6050_mobile_data_collection/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.esp32_mpu6050_mobile_data_collection.ui.theme.Esp32mpu6050mobiledatacollectionTheme
+import com.example.esp32_mpu6050_mobile_data_collection.BLE_reference.ConnectGATTSample
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Esp32mpu6050mobiledatacollectionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ConnectGATTSample()
             }
         }
     }
