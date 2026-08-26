c29bd720c57e50c3e9d22539300390d6b8feb7e1
muz <171587778+MuzammilAijaz@users.noreply.github.com>
2026-08-26 11:24:12 +0500

    feat(data): add data class for storing a complete representation of sensor data
    Includes DMP information (quaternions) along with raw IMU acceleration,
    gyroscope and timestamp information

--- app/src/main/java/com/example/esp32_mpu6050_mobile_data_collection/data/SensorData.kt
+++ app/src/main/java/com/example/esp32_mpu6050_mobile_data_collection/data/SensorData.kt
@@ -1,6 +1,36 @@
 package com.example.esp32_mpu6050_mobile_data_collection.data
 
 sealed interface SensorData {
+    /**
+     * Complete packet received from the MCU.
+     *
+     * This is the canonical representation of a new BLE sample.
+     * The values undergo minimal processing/conversion at the MCU and BLE layers.
+     *
+     * Accelerometer and gyroscope values are preserved as int16_t values from
+     * the MPU6050 and are therefore represented as Short on Android.
+     *
+     * Quaternion values are extracted from the DMP FIFO as int16_t fixed-point
+     * values and converted to normalized floats by dividing by 16384.0f in
+     * the MPU6050 library.
+     */
+    data class FullIMURaw(
+        val ax: Short,
+        val ay: Short,
+        val az: Short,
+
+        val gx: Short,
+        val gy: Short,
+        val gz: Short,
+
+        val qx: Float,
+        val qy: Float,
+        val qz: Float,
+        val qw: Float,
+
+        val timestampUs: Long,
+    ) : SensorData
+
     data class Acceleration(
         val x: Float,
         val y: Float,
